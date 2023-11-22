f70288a474bfa4bc07926807c0211cc60559bf89
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-11-22 09:15:31 -0800

    Update CoinChangeII.java

--- CoinChangeII.java
+++ CoinChangeII.java
@@ -54,3 +54,34 @@ class Solution {
 
     }
 }
+
+
+2. DP (Bottom Up)
+
+ 
+class Solution {
+    //Tc: O(m*n) and Sc: O(m*n) as we are using a dp array with size (m+1)*(n+1)
+    public int change(int amount, int[] coins) {
+        if(coins == null || coins.length == 0) return 0;
+        int m = coins.length;
+        int n = amount;
+        int dp[][] = new int[m+1][n+1];
+        dp[0][0] = 1;
+        for(int i = 1 ; i < dp.length; i++)
+        {
+            for(int j = 0 ; j < dp[0].length; j++)
+            {
+                if(j < coins[i-1])
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else
+                {
+                    dp [i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+
