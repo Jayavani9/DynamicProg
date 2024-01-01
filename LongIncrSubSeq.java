d07c282afa1f4f2ae7d80a32c78d9ce846efc9e9
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2024-01-01 10:27:28 -0800

    Update LongIncrSubSeq.java

--- LongIncrSubSeq.java
+++ LongIncrSubSeq.java
@@ -47,3 +47,52 @@ class Solution {
     }
         
 }
+
+2. Dynamic Programming 
+
+class Solution {
+    //Tc: O(N^2) Sc: O(N) 
+    public int lengthOfLIS(int[] nums) {
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for(int i = 1; i < nums.length; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        int res = 0;
+        for(int c:dp)
+        {
+            res = Math.max(res,c);
+        }
+    return res;
+    }        
+}
+
+
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for(int i = 1; i < nums.length; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                    res = Math.max(res, dp[i]);
+                }
+            }
+        }
+    return res;
+    }
+        
+}
